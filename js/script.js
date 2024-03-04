f80e5456d54384d95415fd66dbefd7ced97029d1
Simon M <smmaecht@gmail.com>
2024-03-04 20:14:22 +0100

    Videos Added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const pageDots = $(".pageNav ul li");
 const pages = $(".page");
+const videos = $("video");
+
+for (const video of videos) {
+    video.load();
+}
 
 var currentPage = 0;
 var scrolling = false;
@@ -20,7 +25,13 @@ for (const dot of pageDots) {
                 block: "end",
                 inline: "nearest",
             });
+            const lastVideo = videos[currentPage - 1];
+            lastVideo != null ? lastVideo.pause() : undefined;
             currentPage = i;
+            const newVideo = videos[currentPage - 1];
+            newVideo != null
+                ? ((newVideo.currentTime = 0), newVideo.play())
+                : undefined;
             updateNavDots();
         }).bind(i, i)
     );
@@ -36,7 +47,13 @@ window.addEventListener("wheel", (e) => {
     if (e.deltaY > 0) {
         //GO DOWN
         if (pages.length > currentPage + 1) {
+            const lastVideo = videos[currentPage - 1];
+            lastVideo != null ? lastVideo.pause() : undefined;
             currentPage++;
+            const newVideo = videos[currentPage - 1];
+            newVideo != null
+                ? ((newVideo.currentTime = 0), newVideo.play())
+                : undefined;
             pages[currentPage].scrollIntoView({
                 behavior: "smooth",
                 block: "end",
@@ -45,9 +62,15 @@ window.addEventListener("wheel", (e) => {
             console.log(currentPage, "GOTO:", pages[currentPage]);
         }
     } else {
-        //GO DOWN
+        //GO UPd
         if (currentPage - 1 >= 0) {
+            const lastVideo = videos[currentPage - 1];
+            lastVideo != null ? lastVideo.pause() : undefined;
             currentPage--;
+            const newVideo = videos[currentPage - 1];
+            newVideo != null
+                ? ((newVideo.currentTime = 0), newVideo.play())
+                : undefined;
             pages[currentPage].scrollIntoView({
                 behavior: "smooth",
                 block: "end",
